02f6ea9a181aa6df0ec9757ac8793a026b0a167e
Pedro Marchiori <pmarchiori08@gmail.com>
2025-05-21 17:36:31 -0300

    salvar alterações locais antes do pull

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
-const express = require('express');
-require('dotenv').config();
-require('./db');
+// const express = require('express');
+// require('dotenv').config();
+// require('./db');
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+// const app = express();
+// const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API funcionando!');
-});
+// app.get('/', (req, res) => {
+//   res.send('API funcionando');
+// });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
--- src/db.js
+++ src/db.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+// const mongoose = require('mongoose');
+// require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Conectado ao MongoDB"))
-.catch(err => console.error("Erro ao conectar:", err));
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+// .then(() => console.log("Conectado ao MongoDB"))
+// .catch(err => console.error("Erro ao conectar:", err));
